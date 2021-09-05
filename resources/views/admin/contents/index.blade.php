869aa52dae23f6d179d4a27014ba4d82ab67cc66
Liam Pugh <78647249+Shutt90@users.noreply.github.com>
2021-09-05 23:04:49 +0100

    Brought images through and added confirm delete

--- resources/views/admin/contents/index.blade.php
+++ resources/views/admin/contents/index.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.header')
+
 @include('admin.layouts.adminside')
 
 <div class="admin-contents">
@@ -12,10 +14,11 @@
             <a href="{{route('content.edit', $content->id)}}">
                 <i class="fas fa-pen-square"></i>
             </a>
+            <img src="{{asset('storage/images/' . $content->image)}}">
             <form method="POST" action="{{route('content.destroy', $content->id)}}">
                 @csrf
                 @method("DELETE")
-                <button type="submit"><i class="fas fa-trash"></i></button>
+                <button onclick="return confirm('Are you sure?')" type="submit"><i class="fas fa-trash"></i></button>
             </form>
         </div>
         @endforeach
