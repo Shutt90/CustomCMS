9597f9f8882f2e91087c5444182c1bb5eef533c3
Liam Pugh <78647249+Shutt90@users.noreply.github.com>
2021-09-18 19:11:43 +0100

    Temporary blog page added

--- resources/views/front/blog.blade.php
+++ resources/views/front/blog.blade.php
@@ -8,14 +8,18 @@
     <div class="blog">
         <div class="blog-title">
 
-            <h3 class="blog-title__txt">Photographer Name</h3>
+            <h3 class="blog-title__txt">Company Name</h3>
             <div class="blog-title__underline"></div>
         </div>
 
         <div class="blog-posts">
-
-        
-            
+            @foreach($blogs as $blog)
+            <div class="blog-posts-item">
+                <h4 class=blog-posts-item__title">
+                    {{$blog->title}}
+                </h4>
+                {{$blog->content}}
+            </div>
         </div>
 
     </div>
