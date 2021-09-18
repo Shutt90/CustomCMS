b43516808f1e11cd9ba21aec0230bbd67d9ebf08
Liam Pugh <78647249+Shutt90@users.noreply.github.com>
2021-09-18 16:53:12 +0100

    Created permission and middleware for admin

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Checks is the user is logged in or if they have admin
+        if(!auth()->check() || !auth()->user()->admin) {
+            abort(403)
+        }
+
+        return $next($request);
+    }
+}
