7fb3e10bec65fd3b678f013feadba6279fb0a37a
Liam Pugh <liam.pugh.009@gmail.com>
2021-10-11 23:21:25 +0100

    Deleted unused paths in test

--- tests/Feature/ContentTest.php
+++ tests/Feature/ContentTest.php
@@ -3,9 +3,6 @@
 namespace Tests\Feature;
 
 use App\Models\Content;
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ContentTest extends TestCase
