bf511e5dffb5ac05da5a3e3aa2b6c0c721d129bd
Liam Pugh <liam.pugh.009@gmail.com>
2021-11-03 20:52:49 +0000

    Spruced up the dashboard a touch

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -9,19 +9,35 @@
 
     <h1 class="dashboard-title">Welcome Back {{Auth::user()->fname}}</h1>
 
-    <div class="dashboard-content">
-
-        <div class="dashboard-screens">
-
-            <div class="row-margin row justify-center">
-                <div class="col-lg-6 col-sm-12">
-                    <canvas class="visitors"></canvas>
-                </div>
-                <div class="col-lg-6 col-sm-12">
-                    <canvas class="current-visitors"></canvas>
-                </div>
+    <div class="dashboard">
+        <div class="dashboard-info">
+            <div class="dashboard-info-sec">
+                Welcome Back<br/>
+                {{Auth::user()->fname}} {{Auth::user()->surname}}
             </div>
-            
+            <div class="dashboard-info-sec">
+                <p class="dashboard-info-sec__text">
+                    <span>Since you last visit there has been</span>
+                    <span>Number</span>
+                    <span>New Visitors</span>
+                </p>
+            </div>
+            <div class="dashboard-info-sec">
+                <p class="dashboard-info-sec__text">
+                    <span>Since you last visit there has been</span>
+                    <span>Number</span>
+                    <span>New Purchases??</span>
+                </p>
+            </div>
+        </div>
+
+        <div class="dashboard-graph">
+            <div class="dashboard-graph-sec">
+                <canvas class="visitors"></canvas>
+            </div>
+            <div class="dashboard-graph-sec">
+                <canvas class="current-visitors"></canvas>
+            </div>
         </div>
 
     </div>
